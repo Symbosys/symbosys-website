598a0696f94406472a07974c34868ed0d05a5aea
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-06-02 16:48:54 +0530

    fixed inital rendereding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,10 +36,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="light" suppressHydrationWarning>
       <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
-          rel="stylesheet"
-        />
         <link rel="icon" href="/logo/symbosys.png" />
         <script
           dangerouslySetInnerHTML={{
